292b768249a0900c6bdedd4def815e6d2b4b6af0
nxs4246 <nxs4246@mavs.uta.edu>
2024-06-13 19:44:15 -0500

    completed 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(left, right) {
+    let result = 0;
+    let max;
+    let min;
 
+    if (left < 0 || right < 0 || typeof left !== 'number' || typeof right !== 'number' || isNaN(left) || isNaN(right)) {
+        return 'ERROR';
+    }
+
+    if(left <= right) {
+        min = left;
+        max = right;
+    } else {
+        max = left;
+        min = right;
+    }
+
+    for(let i = min; i <= max; i++) {
+        result = result + i;
+    }
+    return result;
 };
 
 // Do not edit below this line
