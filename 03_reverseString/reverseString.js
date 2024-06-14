3924137a34104172244760aa2b0bad6f9549cb6d
nxs4246 <nxs4246@mavs.uta.edu>
2024-06-13 19:12:02 -0500

    completed 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let len_string = string.length;
+    let rev_array = "";
 
+    for(let i = (len_string - 1); i >= 0; i--) {
+        rev_array += string[len_string - 1];
+        len_string--;
+    }
+
+    return rev_array;
 };
 
 // Do not edit below this line
