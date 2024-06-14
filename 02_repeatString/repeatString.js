fbec0a83eb4324240e348685f5a6d11965f03a96
nxs4246 <nxs4246@mavs.uta.edu>
2024-06-13 19:01:48 -0500

    completed 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(text, number) {
+    let result = "";
 
+    if(number == 0) {
+        return result;
+    } else if(number < 0) {
+        return "ERROR";
+    } else {
+        for(let i = 1; i <= number; i++) {
+            result += text;
+        }
+        return result;
+    }
 };
 
 // Do not edit below this line
